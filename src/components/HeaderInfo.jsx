506ff5c58bf0b55f5c002f785faa51f58b80add1
OrcaBusinessSolution <support@orcabs.com>
2023-02-21 07:04:59 +0000

    Header Info degisiklikler

--- src/components/HeaderInfo.jsx
+++ src/components/HeaderInfo.jsx
@@ -5,36 +5,32 @@ import EmailIcon from "@mui/icons-material/Email";
 import { Chip, ListItemButton } from "@mui/material";
 
 const InfoDiv = styled.div`
-  background: #fff;
+  background-color: #fff;
   border-bottom: 1px solid #f2f2f2;
   color: #858a99;
-  width: 100%;
-  position: relative;
+ // position: relative;
   font-size: 14px;
   display: flex;
   flex: 1;
   flex-direction: row;
-  & p {
-    margin-left: 20em;
-  }
-  & a {
-    text-decoration: none;
-    color: #858a99;
-    right: -65em;
-  }
+  justify-content: space-between;
+  align-items: center;
+ 
+`;
+const ContactDiv = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: row; 
 `;
 function HeaderInfo() {
   return (
     <InfoDiv>
       <p>Taşkın Otel'e hoş geldiniz</p>
-      <div>
+      <ContactDiv >
         <ListItemButton component="a" href="tel:02847141706">
           <Chip icon={<MdPhone />} size={"small"} label="0284 714 1706" />
           {/* <a href="tel:02847141706">02847141706 </a> */}
         </ListItemButton>
-      </div>
-
-      <div>
         <ListItemButton component="a" href="mailto:info@oteltaskin.com">
           <Chip
             icon={<EmailIcon />}
@@ -43,7 +39,7 @@ function HeaderInfo() {
           />
           {/* <a href="mailto:info@oteltaskin.com">info@oteltaskin.com </a> */}
         </ListItemButton>
-      </div>
+      </ContactDiv>
     </InfoDiv>
   );
 }
