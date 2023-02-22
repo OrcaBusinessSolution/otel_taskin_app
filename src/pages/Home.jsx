baa471ca75e0f174b7790ff901783afb7fdb5e38
OrcaBusınessSolutıon <support@orcabs.com>
2023-02-22 16:37:44 +0300

    added new components to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import styled from "styled-components";
-import InfoDiv from "../components/HeaderInfo";
-import Header from "../components/header"
+import HeaderInfo from "../components/HeaderInfo";
+import Header from "../components/Header";
+import Slider from "../components/Slider";
+import Rooms from "../components/Rooms";
+import Footer from "../components/Footer";
 
 const Container = styled.div``;
 
 function Home() {
   return (
     <Container>
-      <InfoDiv />
-      <Header/>
+      <HeaderInfo />
+      <Header />
+      <Slider />
+      <Rooms />
+      <Footer />
     </Container>
   );
 }
