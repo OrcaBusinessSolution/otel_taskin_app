955ef645817713bbfd3f4ee7122f93bfaf49a92d
OrcaBusınessSolutıon <support@orcabs.com>
2023-02-22 16:34:07 +0300

    created header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,50 @@
 import { Link } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
-import logo from "../pictures/logo.png";
 
 const Container = styled.div`
-  width: 100%;
-  flex-direction: row;
-  flex: 1;
   display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  font-family: Roboto, sans-serif;
+  font-size: 17px;
+  font-weight: 1000;
+  padding: 2rem;
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const HeaderDiv = styled.div`
-  width: 100%;
-  text-align: center;
-  align-items: center;
-  letter-spacing: 0.05em;
-  text-transform: uppercase;
-  flex-direction: row;
-  flex: 1;
-  display: flex;
-  margin: 3em 40em;
-  background-color: #000;
+  margin-right: 2rem;
+  & a {
+    color: white;
+    letter-spacing: 0.1em;
+    margin-left: 1.5em;
+    color: #606060;
+  }
+  & a:hover {
+    color: #c12200;
+    font-size: 18px;
+  }
 `;
 
-const Image = styled.img`
-  width: 300px;
-  margin-top:1em;
-  margin-left:em;
+const ImageDiv = styled.img`
+  height: 4em;
+  margin-left: 2rem;
 `;
 
 function Header() {
   return (
     <Container>
-      <Image src={logo}></Image>
+      <Link href="http://oteltaskin.com/">
+        <ImageDiv src={"./pictures/logo.png"} alt="otel, taşkın otel, keşan"></ImageDiv>
+      </Link><hr/>
       <HeaderDiv>
         <Link href="#" underline="none">
-          ANA SAYFA
+          ANASAYFA
         </Link>
         <Link href="#" underline="none">
           ODALAR
@@ -55,37 +64,3 @@ function Header() {
 }
 
 export default Header;
-
-{
-  /* <Header>
-<div id="logo">
-  <Link href="http://oteltaskin.com/" underline="none">
-    <Image src={logo} alt="otel, taşkın otel, keşan" width="260em" />
-  </Link>
-</div>
-<div>
-  <Link href="#" underline="none"> ANA SAYFA</Link>
-</div>
-<div>
-  <Link href="#" underline="none">ODALAR</Link>
-</div>
-<div>
-  <Link href="#" underline="none">GALERİ</Link>
-</div>
-<div>
-  <Link href="#" underline="none">REZERVASYON</Link>
-</div>
-<div>
-  <Link href="#" underline="none">İLETİŞİM</Link>
-</div>
-</Header> */
-}
-
-// & div#logo {
-//   margin-top: -1em;
-//   margin-left: -18em;
-//   width: 20em;
-// }
-// & div {
-//   padding: 1em;
-// }
