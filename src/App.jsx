d83ad1161d0d0c8b66e8f58ff55106d5555ba682
OrcaBusınessSolutıon <support@orcabs.com>
2023-02-23 14:44:05 +0300

    created new pages with links for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import Home from './pages/Home'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Communication from "./pages/Communication";
+import Gallery from "./pages/Gallery";
+import Home from "./pages/Home";
+import Reservation from "./pages/Reservation";
+import Rooms from "./pages/Rooms";
 
 export default function App() {
   return (
-    <div>
-      <Home/>
-    </div>
-  )
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="rooms" element={<Rooms />} />
+      <Route path="gallery" element={<Gallery />} />
+      <Route path="reservation" element={<Reservation />} />
+      <Route path="communication" element={<Communication />} />
+    </Routes>
+  );
 }
